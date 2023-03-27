71c55150db4d1f9c92230bf6329d991b7bf64497
Kamran030 <kamranmammadov030@gmail.com>
2023-03-27 18:00:10 +0400

    use mysql database

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    db:{
+        host:"localhost",
+        user:"root",
+        password:"kamran120",
+        database:"blogapp"
+    }
+}
+module.exports = config;
--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,77 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../data/db");
 
-const path = require("path");
+const data = {
+  title: "Popüler Kurslar",
+  categories: [
+    "Web Geliştirme",
+    "Programlama",
+    "Mobil Uygulamalar",
+    "Veri Analizi",
+    "Ofis Uygulamaları",
+  ],
+  blogs: [
+    {
+      blogid: 1,
+      baslik: "Komple Uygulamalı Web Geliştirme",
+      aciklama:
+        "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
+      resim: "1.jpeg",
+      anasayfa: true,
+      onay: true,
+    },
+    {
+      blogid: 2,
+      baslik: "Python ile Sıfırdan İleri Seviye Python Programlama",
+      aciklama:
+        "Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
+      resim: "2.jpeg",
+      anasayfa: true,
+      onay: false,
+    },
+    {
+      blogid: 3,
+      baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
+      aciklama:
+        "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
+      resim: "3.jpeg",
+      anasayfa: false,
+      onay: true,
+    },
+    {
+      blogid: 3,
+      baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
+      aciklama:
+        "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
+      resim: "3.jpeg",
+      anasayfa: false,
+      onay: true,
+    },
+  ],
+};
 
-router.use("/blog/:blogid", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/users","blog-details.html"));
+router.use("/blogs/:blogid", function (req, res) {
+  res.render("users/blog-details");
 });
 
-router.use("/blogs", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/users","blogs.html"));
+router.use("/blogs", function (req, res) {
+  res.render("users/blogs", data);
 });
 
-router.use("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/users","index.html"));
+router.use("/", function (req, res) {
+  db.execute("SELECT * FROM blog")
+    .then((result) => {
+      console.log(result[0]);
+      res.render("users/index", {
+        title: "Popüler Kurslar",
+        categories: data.categories,
+        blogs: result[0],
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
